f03099ef6234afb42be1fa7e82eb2a92a93c08eb
Rafael Cuello <rafaelcuello@docente.edib.es>
2020-04-23 18:10:04 +0200

    Añadido How are youu?

--- Program.vb
+++ Program.vb
@@ -3,5 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World! How are you?")
+        Console.WriteLine("My name is HolaMundo?")
     End Sub
 End Module
